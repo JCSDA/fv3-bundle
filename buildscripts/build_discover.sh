f553308a6bc8776c6b6fa2459e7265efdb407842
danholdaway <danholdaway@users.noreply.github.com>
2020-05-29 13:22:37 -0400

    add gfdl_atmos_cubed_sphere

--- buildscripts/build_discover.sh
+++ buildscripts/build_discover.sh
@@ -3,13 +3,14 @@
 set -e
 
 # Usage of this script.
-usage() { echo "Usage: $(basename $0) [-c intel-impi/20.0.0.166|intel-impi/19.1.0.166|gnu-impi/9.2.0|baselibs/intel-impi/19.1.0.166] [-b debug|release] [-m default|geos|gfs] [-n 1..12] [-t ON|OFF] [-x] [-v] [-h]" 1>&2; exit 1; }
+usage() { echo "Usage: $(basename $0) [-c intel-impi/20.0.0.166|intel-impi/19.1.0.166|gnu-impi/9.2.0|baselibs/intel-impi/19.1.0.166] [-b debug|release] [-m fv3|geos|gfs|ufs] [-l ON|OFF (linear model)] [-n 1..12] [-t ON|OFF] [-x] [-v] [-h]" 1>&2; exit 1; }
 
 # Set input argument defaults.
 compiler="intel-impi/20.0.0.166"
 build="debug"
 clean="NO"
-model="default"
+model="fv3"
+linearmodel="ON"
 nthreads=12
 run_ctest="ON"
 verbose="OFF"
@@ -34,9 +35,15 @@ while getopts 'v:t:xhc:b:m:n:' OPTION; do
         ;;
     m)
         model="$OPTARG"
-        [[ "$model" == "default" || \
+        [[ "$model" == "fv3" || \
            "$model" == "geos" || \
-           "$model" == "gfs" ]] || usage
+           "$model" == "gfs" || \
+           "$model" == "ufs" ]] || usage
+        ;;
+    l)
+        linearmodel="$OPTARG"
+        [[ "$lienarmodel" == "ON" || \
+           "$lienarmodel" == "OFF" ]] || usage
         ;;
     n)
         n="$OPTARG"
@@ -68,15 +75,16 @@ done
 shift "$(($OPTIND -1))"
 
 echo "Summary of input arguments:"
-echo "   build = $build"
-echo "compiler = $compiler"
-echo "   model = $model"
-echo " threads = $nthreads"
-echo "   ctest = $run_ctest"
-echo "   clean = $clean"
-echo " verbose = $verbose"
-echo " account = $account"
-echo "   queue = $queue"
+echo "      build = $build"
+echo "   compiler = $compiler"
+echo "      model = $model"
+echo "linearmodel = $linearmodel"
+echo "    threads = $nthreads"
+echo "      ctest = $run_ctest"
+echo "      clean = $clean"
+echo "    verbose = $verbose"
+echo "    account = $account"
+echo "      queue = $queue"
 echo
 
 # Load JEDI modules.
@@ -92,21 +100,35 @@ module list
 
 # Set up model specific paths for ecbuild.
 case "$model" in
-    "default" )
-        MODEL=""
+    "fv3" )
+        MODEL="-DBUILD_WITH_MODEL=FV3CORE"
         ;;
     "geos" )
         read -p "Enter the path for GEOS model [e.g: /discover/nobackup/GEOSgcm/install/] " GEOS_PATH
-        MODEL="-DBUILD_WITH_GEOS=ON -DGEOS_PATH=$GEOS_PATH"
+        MODEL="-DBUILD_WITH_MODEL=GEOS -DGEOS_PATH=$GEOS_PATH"
         ;;
     "gfs" )
-        MODEL="-DBUILD_WITH_GFS=ON"
+        MODEL="-DBUILD_WITH_MODEL=GFS"
+        ;;
+    "ufs" )
+        MODEL="-DBUILD_WITH_MODEL=UFS"
+        ;;
+esac
+
+case "$linearmodel" in
+    "ON" )
+        LINEARMODEL="-DBUILD_WITH_LINEARMODEL=ON"
+        lmflag="lmodon"
+        ;;
+    "OFF" )
+        LINEARMODEL="-DBUILD_WITH_LINEARMODEL=OFF"
+        lmflag="lmodoff"
         ;;
 esac
 
 # Set up FV3JEDI specific paths.
 compiler_build=`echo $compiler | tr / -`
-FV3JEDI_BUILD="$PWD/build-$compiler_build-$build-$model"
+FV3JEDI_BUILD="$PWD/build-$compiler_build-$build-$model-$lmflag"
 cd $(dirname $0)/..
 FV3JEDI_SRC=$(pwd)
 
@@ -136,7 +158,7 @@ sed -i "s,BUILDDIR,$FV3JEDI_BUILD,g" $file
 
 # Build
 # -----
-ecbuild --build=$build -DMPIEXEC=$MPIEXEC $MODEL $FV3JEDI_SRC
+ecbuild --build=$build -DMPIEXEC=$MPIEXEC $MODEL $LINEARMODEL $FV3JEDI_SRC
 make update
 cd fv3-jedi
 make -j$nthreads
