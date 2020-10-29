334003343ad6a339a8efa6b428d98fe3396c0f37
mmiesch <miesch@ucar.edu>
2020-10-29 03:26:13 +0000

    use plot_from_ioda_hofx.py instead of iodaplots

--- tutorials/Hofx/run.bash
+++ tutorials/Hofx/run.bash
@@ -14,7 +14,7 @@ function get_dir {
 
 # ---------------------------
 # get desired instrument
-instrlist=(Aircraft Amsua_n19 Atms_n20 Cris_n20 GnssroBnd Radiosonde Satwinds)
+instrlist=(Aircraft Amsua_n19 Atms_n20 GnssroBnd Radiosonde Satwinds Medley)
 
 if [ $# -ne 1 ]; then
    echo "Usage: "
@@ -49,9 +49,9 @@ echo "JEDI build directory = "${JEDI_BUILD_DIR}
 
 # link to crtm coefficents
 mkdir -p Data
-ln -sf ${JEDI_BUILD_DIR}/fv3-jedi/test/Data/crtm Data/crtm
-ln -sf ${JEDI_BUILD_DIR}/test_data/crtm/2.3.0/SpcCoeff/Little_Endian/* Data/crtm
-ln -sf ${JEDI_BUILD_DIR}/test_data/crtm/2.3.0/TauCoeff/ODPS/Little_Endian/* Data/crtm
+cp -r ${JEDI_BUILD_DIR}/fv3-jedi/test/Data/crtm Data/crtm
+cp ${JEDI_BUILD_DIR}/test_data/crtm/2.3.0/SpcCoeff/Little_Endian/* Data/crtm
+cp ${JEDI_BUILD_DIR}/test_data/crtm/2.3.0/TauCoeff/ODPS/Little_Endian/* Data/crtm
 
 # Create directories to store output
 # --------------------------------
@@ -71,20 +71,32 @@ export OMP_NUM_THREADS=1
 # Run the Hofx application
 
 if [[ ${instrument} == Aircraft ]]; then
-    fileprefix=hofx3d_gfs_c48_ncdiag_aircraft_
+    prefix=hofx3d_gfs_c48_ncdiag_aircraft_PT6H_20201001_0300Z
+    varlist=(air_temperature eastward_wind northward_wind specific_humidity)
 elif [[ ${instrument} == Amsua_n19 ]]; then
-    fileprefix=hofx3d_gfs_c48_ncdiag_amsua-n19_
+    prefix=hofx3d_gfs_c48_ncdiag_amsua-n19_PT6H_20201001_0300Z
+    varlist=(brightness_temperature_12)
 elif [[ ${instrument} == Atms_n20 ]]; then
-    fileprefix=hofx3d_gfs_c48_ncdiag_atms-n20_
-elif [[ ${instrument} == Cris_n20 ]]; then
-    fileprefix=hofx3d_gfs_c48_ncdiag_cris-n20_
+    prefix=hofx3d_gfs_c48_ncdiag_atms-n20_PT6H_20201001_0300Z
+    varlist=(brightness_temperature_1 brightness_temperature_2 brightness_temperature_3 \
+             brightness_temperature_4 brightness_temperature_5 brightness_temperature_6 \
+             brightness_temperature_7 brightness_temperature_5 brightness_temperature_6 \
+             brightness_temperature_10 brightness_temperature_11 brightness_temperature_12 \
+             brightness_temperature_13 brightness_temperature_14 brightness_temperature_15 \
+             brightness_temperature_16 brightness_temperature_17 brightness_temperature_18 \
+             brightness_temperature_19 brightness_temperature_20 brightness_temperature_21 \
+             brightness_temperature_22)
 elif [[ ${instrument} == GnssroBnd ]]; then
-    fileprefix=hofx3d_gfs_c48_nomads_gnssro_
+    prefix=hofx3d_gfs_c48_nomads_gnssro_PT6H_20201001_0300Z
+    varlist=(bending_angle)
 elif [[ ${instrument} == Radiosonde ]]; then
-    fileprefix=hofx3d_gfs_c48_ncdiag_radiosonde_
+    prefix=hofx3d_gfs_c48_ncdiag_radiosonde_PT6H_20201001_0300Z
+    varlist=(air_temperature eastward_wind northward_wind)
 elif [[ ${instrument} == Satwinds ]]; then
-    fileprefix=hofx3d_gfs_c48_ncdiag_radiosonde_
+    prefix=hofx3d_gfs_c48_ncdiag_satwind_PT6H_20201001_0300Z
+    varlist=(air_temperature eastward_wind northward_wind)
 else
+    echo "Medley option is run only: not generating plots"
     exit 0
 fi
 
@@ -93,13 +105,20 @@ application=gfs
 mpirun -n 12 $jedibin/fv3jedi_hofx_nomodel.x config/${instrument}_${application}.hofx3d.jedi.yaml
 
 # ------------------------------------------------------
-# Bin the output
+# Bin the output to get obs counts
 
-iodaplots bin output/hofx/${fileprefix}* -o output/binned/${instrument}.nc4 -c config/${instrument}_gfs.plot.yaml
+iodaplots bin output/hofx/${prefix}* -o output/binned/${instrument}.nc4 -c config/${instrument}_gfs.plot.yaml
+iodaplots plot -e ${instrument} output/binned/${instrument}.nc4 -o output/plots/${instrument}/- -c config/${instrument}_gfs.plot.yaml
 
 # ------------------------------------------------------
 # Make the plots
 
-iodaplots plot -e ${instrument} output/binned/${instrument}.nc4 -o output/plots/${instrument}/ -c config/${instrument}_gfs.plot.yaml
+cd output/plots/${instrument}
+
+for var in "${varlist[@]}"; do
+   ../../../plot_from_ioda_hofx.py --hofxfiles ../../hofx/${prefix}_NPROC.nc4 --variable ${var}@ObsValue --nprocs 12 --window_begin 2020100103
+   ../../../plot_from_ioda_hofx.py --hofxfiles ../../hofx/${prefix}_NPROC.nc4 --variable ${var}@hofx --nprocs 12 --window_begin 2020100103
+   ../../../plot_from_ioda_hofx.py --hofxfiles ../../hofx/${prefix}_NPROC.nc4 --variable ${var}@hofx --omb=True --nprocs 12 --window_begin 2020100103
+done
 
 exit 0
